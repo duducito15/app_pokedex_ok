a02a003acd06a42a3a76414d01d03c44d3b8d8d9
Eduardo Chavez <echavezv@utea.edu.pe>
2023-10-27 08:02:56 -0500

    GridView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Text(
+                  "Pokedex",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12.0,
+                  crossAxisSpacing: 12.0,
+                  childAspectRatio: 1.7,
+                  children: [
+                    Container(
+                      color: Colors.amber,
+                    ),
+                    Container(
+                      color: Colors.black,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
